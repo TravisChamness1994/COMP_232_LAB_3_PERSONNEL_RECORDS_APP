58367ae961a62793a15eb69ac0a30c188c9840f8
TravisChamness1994 <travisdanfordc@yahoo.com>
2020-02-21 18:01:59 -0800

    2/21/2020 Done inputPersonalData. Dont forget to set address when inputting with scanf.

--- lab3_personnel/src/person.c
+++ lab3_personnel/src/person.c
@@ -7,14 +7,21 @@
 
 LIST *head = NULL, *tail = NULL;
 
+//name age height mm/dd/yyyy
 void inputPersonalData(PERSON *person)
 {
-    // TODO implement the function
+    //Input personal data to each section
+    scanf("%s", person->name);
+    scanf("%d", &person->age);
+    scanf("%f", &person->height);
+    scanf("%d/%d/%d", &person->bday.month, &person->bday.day, &person->bday.year);
+    // TODO implement the function - inputPersonalData - Work
     // or check out part 9 of the c tutorial ;)
 }
 
 void addPersonalDataToDatabase(PERSON *person)
 {
+
     // TODO Implement the function
 }
 
